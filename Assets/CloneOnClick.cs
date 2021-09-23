318d6e94e6246bdee817948b00ce1d3ac1df1915
Anais-cmb <cambou.anais@gmail.com>
2021-09-23 11:41:44 +0200

    update clone on click

--- Assets/CloneOnClick.cs
+++ Assets/CloneOnClick.cs
@@ -5,17 +5,6 @@ using UnityEngine;
 public class CloneOnClicks : MonoBehaviour
 {
     public int numberOfClones = 3 ;
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnMouseDown() {
         // La Mort !
