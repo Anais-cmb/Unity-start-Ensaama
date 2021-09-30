7b417b641eb7572e3552ae434cc7ea257262f783
Anais-cmb <cambou.anais@gmail.com>
2021-09-30 12:20:01 +0200

    push

--- Assets/SCRIPTS/Eye.cs
+++ Assets/SCRIPTS/Eye.cs
@@ -26,8 +26,18 @@ public class Eye : MonoBehaviour
 
             // "traînée angulaire, c-à-d la friction associée à la rotation
             // 0.05 par défaut, nous mettons donc 0.2f pour ralentir les yeux.
-            body.angularDrag = 0.2f;            
-        }
+            body.angularDrag = 0.2f; 
 
-        
+            Debug.Log("i'm falliiiiing!!");
+            StartCoroutine (FallEnd());           
+        }
+        //C'est pénible, mais c'est ainsi dans unity, pour retarder une execution
+        // il faut créer une fonction de type "IEnumerator"
+        // c'est comme un delay (de 4secondes) [si dessous]
+        IEnumerator FallEnd () {
+            yield return new WaitForSeconds (4f);
+            isFalling = false;
+
+            Debug.Log ("Well, finally it's ok, i'm still alive");
+        }
 }
