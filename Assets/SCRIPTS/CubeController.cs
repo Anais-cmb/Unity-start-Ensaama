96c534b8d11a83f6d1c7bf65d2271b80f4ef1c75
Anais-cmb <cambou.anais@gmail.com>
2021-10-07 09:52:09 +0200

    push

--- Assets/SCRIPTS/CubeController.cs
+++ Assets/SCRIPTS/CubeController.cs
@@ -6,22 +6,43 @@ public class CubeController : MonoBehaviour
 {
     public float speed = 5f;
     public float jumpSpeed = 10f;
+    public float jumpAngularSpeed = 5f;
     public float jumpCoolDown = 0f;
-    void Update()
-    {
-        Rigidbody body = GetComponent<Rigidbody>();
+    public Rigidbody body;
+
+    void Start() {
+        body = GetComponent<Rigidbody>();
+
+        if (body == null) {
+            Debug.LogError("Il n'y a pas de RigidBody sur le GameObject. Pense à en ajouter un.");
+            Debug.Break();
+        }
+    }
+
+    void UpdateMove() {
         Vector3 move = new Vector3();
         move.x = speed * Input.GetAxis("Horizontal");
         move.y = body.velocity.y;
         move.z = speed * Input.GetAxis("Vertical");
-
+        
+        body.velocity = move;
+    }
+    void UpdateJump (){
+        
         jumpCoolDown = jumpCoolDown - Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.Space) && jumpCoolDown <= 0) {
             jumpCoolDown = 1f;
+            Vector3 move = body.velocity;
             move.y = jumpSpeed;
-        }
+            body.velocity = move;
 
-        body.velocity = move;
-        // transform.position = transform.position + Vector3.right * 0.2f;
+            body.angularVelocity = Random.onUnitSphere * jumpAngularSpeed;
+        }
     }
+
+    void Update(){
+        UpdateMove();
+        UpdateJump();
+    }
+
 }
