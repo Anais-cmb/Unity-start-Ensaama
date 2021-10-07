88b8bf380b18fa3b2add7fa0bf463ed26cce5a6d
Anais-cmb <cambou.anais@gmail.com>
2021-10-07 12:19:37 +0200

    push

---
+++ Assets/SCRIPTS/CrownChildCloner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CroneChildCloner : MonoBehaviour
+{
+    public int numberOfClones = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject clone = Instantiate(gameObject, transform.parent);
+        Destroy(clone.GetComponent<CrownChildCloner>());
+        float yAngle = Random.Range(0f,360f);
+        clone.transform.Rotate(0, yAngle, 0);
+    }
+}
