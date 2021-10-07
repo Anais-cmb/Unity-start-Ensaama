62d561dd719449b2600ee5166768df5c44e76d82
Anais-cmb <cambou.anais@gmail.com>
2021-10-07 11:48:58 +0200

    push

---
+++ Assets/SCRIPTS/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Vector3 rotationSpeed = new Vector3();
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotate(rotationSpeed * Time.deltaTime);
+    }
+}
