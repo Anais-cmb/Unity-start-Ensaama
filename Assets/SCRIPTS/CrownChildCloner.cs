cf4b2bc1b4e86f786786943c1f99335383953a72
Anais-cmb <cambou.anais@gmail.com>
2021-10-07 12:34:28 +0200

    push

--- Assets/SCRIPTS/CrownChildCloner.cs
+++ Assets/SCRIPTS/CrownChildCloner.cs
@@ -7,11 +7,34 @@ public class CroneChildCloner : MonoBehaviour
     public int numberOfClones = 10;
 
     // Update is called once per frame
-    void Update()
-    {
+    void Start(){
+
+        for (int i = 0; i < numberOfClones; i = i + 1){
+            MakeAClone();
+        }
+    }
+
+    void MakeAClone() {
         GameObject clone = Instantiate(gameObject, transform.parent);
+
+        // IL FAUT DETRUIRE LE SCRIPT SUR L ENFANT !!!
+        // Sans cela celui se clonera à son tour,puis son enfant aussi, puis le petit-enfant etc..
         Destroy(clone.GetComponent<CrownChildCloner>());
+
         float yAngle = Random.Range(0f,360f);
         clone.transform.Rotate(0, yAngle, 0);
+
+        // Changeons la vitesse de rotation du clone
+        Rotate rotate = clone.GetComponent<Rotate>();
+        rotate.rotationSpeed = new Vector3(
+            0f, 
+            Random.Range(20f, 30f), 
+            0f
+        );
+
+        // Changeons la taille de l'enfant du clone
+        clone.transform.GetChild(0).localScale = Vector3.one * Random.range(0.2f, 1f) ;
     }
 }
+
+
